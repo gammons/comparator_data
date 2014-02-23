fb86d85fbcd27daab0a7f46a1d3476c356a1bb05
Grant Ammons <gammons@gmail.com>
2014-02-23 16:07:04 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ComparatorData::Application.config.secret_key_base = '6c526355f692354ca2ecdf6af8a75880054849d0fb994a18209a001701d631963787fd3ff564f603db34c26a48199d0ce638f85c525081a0855581c972b58a20'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ComparatorData::Application.config.session_store :cookie_store, key: '_comparator_data_session'
